eb018df7341cb5bb873e812f20b839d5beb66022
DESKTOP-LKGJI17\WIN10 <anakhutankesayangan.3@gmail.com>
2026-02-15 09:07:07 +0700

    up projeck card dengan nama

--- resources/views/siswa/aspirasi.blade.php
+++ resources/views/siswa/aspirasi.blade.php
@@ -22,6 +22,7 @@
 
                         <div class="card-body">
                             <span class="badge bg-primary">{{ $a->kategori->nama }}</span>
+                            <h6 class="mt-2">{{ $a->siswa->nama }}</h6>
                             <h5 class="mt-2">{{ $a->lokasi }}</h5>
                             <p>{{ $a->ket }}</p>
 
